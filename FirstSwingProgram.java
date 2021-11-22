6e23def84aba5814ed4e99cb61d9452092fe368d
harendraprajapati72 <harendraprajapati72@gmail.com>
2021-11-22 23:50:06 +0530

    first

---
+++ FirstSwingProgram.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class FirstSwingProgram {
+    public static void main(String[] args) {
+        JFrame f=new JFrame();
+        JButton B=new JButton("click here");
+        B.setBounds(130,100,100,40);
+        f.add(B);
+        f.setSize(400,500);
+        f.setLayout(null);
+        f.setVisible(true);
+
+        
+    }
+    
+}
